0762082df53d631e356b4be7757aeab8e6266cf6
Config Server <config@example.com>
2025-08-21 11:11:58 +0300

    реализовал логику импорта заметок

--- NewApp/src/main/java/by/koval/importApp/model/Import.java
+++ NewApp/src/main/java/by/koval/importApp/model/Import.java
@@ -1,5 +1,6 @@
 package by.koval.importApp.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,8 +20,9 @@ public class Import {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(name = "last_import_datetime")
-    private LocalDateTime lastImportDateTime;
+    @Column(name = "import_datetime")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime importDateTime;
     @Column(name = "notes_number_imported")
     private int notesNumberImported;
 }
--- NewApp/src/main/java/by/koval/importApp/service/ImportServiceImpl.java
+++ NewApp/src/main/java/by/koval/importApp/service/ImportServiceImpl.java
@@ -3,6 +3,7 @@ package by.koval.importApp.service;
 import by.koval.importApp.dto.OldClientDto;
 import by.koval.importApp.dto.OldNoteDto;
 import by.koval.importApp.dto.ReqParams;
+import by.koval.importApp.model.Import;
 import by.koval.importApp.model.Note;
 import by.koval.importApp.model.Patient;
 import by.koval.importApp.model.User;
@@ -18,7 +19,6 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +34,7 @@ public class ImportServiceImpl implements ImportService {
 
     @Override
     public void importNotes() {
+        int notesImported = 0;
         List<OldClientDto> oldClientsForCurrentImport = getOldClientsForCurrentImport();
         LocalDateTime dateFrom = getLastImportTime();
         LocalDateTime dateTo = LocalDateTime.now();
@@ -67,59 +68,65 @@ public class ImportServiceImpl implements ImportService {
             List<OldNoteDto> oldNotesForCurrentClientGuid = Arrays.asList(Objects.requireNonNull(httpClientService
                     .postRequest(oldAppUrl + "/notes", reqParams, OldNoteDto[].class).getBody()));
 
-//            oldNotesForCurrentClientGuid.parallelStream()
-//                    .map(n -> proceedOldNoteForClientGuid(n, n.getClientGuid()))
-//                    .toList();
+            for (OldNoteDto oldNote : oldNotesForCurrentClientGuid) {
+                notesImported+=proceedOldNoteForClientGuid(oldNote);
+            }
         }
-
-
-
-    }
-
-    private Note mapNote(OldNoteDto oldNoteDto, int patientId) {
-        Note note = new Note();
-        note.setNote(oldNoteDto.getComments());
-        note.setLastModifiedDateTime(oldNoteDto.getModifiedDateTime());
-        note.setCreatedDateTime(oldNoteDto.getCreatedDateTime());
-        note.setPatientId(patientId);
-        if (usersRepository.findByLogin(oldNoteDto.getLoggedUser()).isEmpty()) {
-            usersRepository.save(User.builder().login(oldNoteDto.getLoggedUser()).build());
-        }
-        note.setCreatedByUserId(usersRepository.save(User.builder().login(oldNoteDto.getLoggedUser()).build()).getId());
-
-        return note;
+        importsRepository.save(Import.builder()
+                .notesNumberImported(notesImported)
+                .importDateTime(dateTo)
+                .build());
     }
 
     private LocalDateTime getLastImportTime() {
         LocalDateTime lastImportTime = LocalDateTime.of(1970, 1, 1, 0, 0);
         if (importsRepository.findTopByOrderByLastImportDateTimeDesc().isPresent()) {
-            lastImportTime = importsRepository.findTopByOrderByLastImportDateTimeDesc().get().getLastImportDateTime();
+            lastImportTime = importsRepository.findTopByOrderByLastImportDateTimeDesc().get().getImportDateTime();
         }
         return lastImportTime;
     }
 
     private List<OldClientDto> getOldClientsForCurrentImport() {
-        List<OldClientDto> oldClients = Arrays.asList(Objects.requireNonNull(httpClientService.postRequest(oldAppUrl + "/clients", OldClientDto[].class).getBody()));
+        List<OldClientDto> oldClients = Arrays.asList(Objects.requireNonNull(httpClientService
+                .postRequest(oldAppUrl + "/clients", OldClientDto[].class).getBody()));
         List<Patient> patientsForImport = patientsRepository.findAllByStatusIdIn(List.of(200, 210, 230));
-        List<String> patientsOldGuidsForImport = patientsForImport.stream().map(Patient::getOldClientGuids).flatMap(List::stream).toList();
-        oldClients = oldClients.stream().filter(c -> patientsOldGuidsForImport.contains(c.getGuid())).toList();
+        List<String> patientsOldGuidsForImport = patientsForImport.stream()
+                .map(Patient::getOldClientGuids)
+                .flatMap(List::stream)
+                .toList();
+        oldClients = oldClients.stream()
+                .filter(c -> patientsOldGuidsForImport.contains(c.getGuid()))
+                .toList();
         return oldClients;
     }
 
-    private void proceedOldNoteForClientGuid(OldNoteDto oldNoteForClientGuid, String clientGuid) {
-        Patient patient = patientsRepository.findByOldClientGuidsContains(clientGuid);
-        Optional<Note> note = notesRepository.findByPatientIdIsAndCreatedDateTimeEquals(patient.getId(), oldNoteForClientGuid.getCreatedDateTime());
-        if (note.isPresent() && note.get().getLastModifiedDateTime().isBefore(oldNoteForClientGuid.getModifiedDateTime())) {
-            Note newNote = mapNote(oldNoteForClientGuid, patient.getId());
-            newNote.setId(note.get().getId());
-            if (usersRepository.findByLogin(oldNoteForClientGuid.getLoggedUser()).isEmpty()) {
-                newNote.setLastModifiedByUserId(usersRepository.save(User.builder().login(oldNoteForClientGuid.getLoggedUser()).build()).getId());
-            } else {
-                newNote.setLastModifiedByUserId(usersRepository.findByLogin(oldNoteForClientGuid.getLoggedUser()).get().getId());
-            }
-            notesRepository.save(newNote);
-        } else {
-            notesRepository.save(mapNote(oldNoteForClientGuid, patient.getId()));
+    private int proceedOldNoteForClientGuid(OldNoteDto oldNoteForClientGuid) {
+        int notesImported = 0;
+        Patient patient = patientsRepository.findByOldClientGuidsContains(oldNoteForClientGuid.getClientGuid());
+        User user = usersRepository.findByLogin(oldNoteForClientGuid.getLoggedUser()).orElse(null);
+        if (user == null) {
+            user = usersRepository.save(User.builder().login(oldNoteForClientGuid.getLoggedUser()).build());
         }
+        Note note = notesRepository.findByPatientIdIsAndCreatedDateTimeEquals(patient.getId(),
+                oldNoteForClientGuid.getCreatedDateTime()).orElse(null);
+        if (note == null) {
+            note = Note.builder()
+                    .createdDateTime(oldNoteForClientGuid.getCreatedDateTime())
+                    .note(oldNoteForClientGuid.getComments())
+                    .lastModifiedDateTime(oldNoteForClientGuid.getModifiedDateTime())
+                    .createdByUserId(user.getId())
+                    .lastModifiedByUserId(user.getId())
+                    .patientId(patient.getId())
+                    .build();
+            notesRepository.save(note);
+            notesImported++;
+        } else if (note.getLastModifiedDateTime().isBefore(oldNoteForClientGuid.getModifiedDateTime())) {
+            note.setLastModifiedDateTime(oldNoteForClientGuid.getModifiedDateTime());
+            note.setLastModifiedByUserId(user.getId());
+            note.setNote(oldNoteForClientGuid.getComments());
+            notesRepository.save(note);
+            notesImported++;
+        }
+        return notesImported;
     }
 }
